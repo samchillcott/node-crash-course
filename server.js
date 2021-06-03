73bd01d66b1865c06e6b7873d757f93133c876cb
Sam Chillcott <samchillcott@gmail.com>
2021-06-03 15:56:21 +0100

    add basic html to send to browser

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
-    res.setHeader('Content-Type', 'text/plain/');
-    res.write('oi oi!');
-    res.end();
+    res.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('./views/index.html', (err, data) => {
+       if (err) {
+           console.log(err);
+           res.end();
+       } else {
+        //    res.write(data);
+           res.end(data);
+       }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
