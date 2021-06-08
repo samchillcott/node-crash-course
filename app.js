c5cfc35bfdf4385431bc085aaabe9e1ace6d4465
Sam Chillcott <samchillcott@gmail.com>
2021-06-08 15:40:37 +0100

    add blogs array and output blog content

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Home'});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+    res.render('index', {title: 'Home', blogs});
 })
 
 app.get('/about', (req, res) => {
