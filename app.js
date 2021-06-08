9f786b6db3dd1902024db91cb7504e82eab578b7
Sam Chillcott <samchillcott@gmail.com>
2021-06-08 12:31:54 +0100

    add 404 remove raw node file

--- app.js
+++ app.js
@@ -15,3 +15,7 @@ app.get('/about', (req, res) => {
 app.get('/about-us', (req, res) => {
     res.redirect('/about')
 })
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname})
+})
