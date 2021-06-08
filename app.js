1a8165d2fcf258d6d6b5b3ca84ad4624c923fe07
Sam Chillcott <samchillcott@gmail.com>
2021-06-08 12:11:38 +0100

    add nav and redirect in express

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home Page</p>')
     res.sendFile('./views/index.html', { root: __dirname})
 })
-app.get('/about', (req, res) => {
-    // res.send('<p>About Page</p>')
-    res.sendFile('./views/about.html', { root: __dirname})
 
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname})
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
 })
