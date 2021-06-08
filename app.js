bc18af4558026d534be3c8f6553ab31f1f3d1df4
Sam Chillcott <samchillcott@gmail.com>
2021-06-08 13:32:05 +0100

    add ejs files and renders

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname})
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname})
+    res.render('about');
 })
 
 app.get('/about-us', (req, res) => {
@@ -17,5 +19,5 @@ app.get('/about-us', (req, res) => {
 })
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname})
+    res.status(404).render('404');
 })
