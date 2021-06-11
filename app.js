b8b322c5a32103a9491cb59196c9dc52304b0cb0
Sam Chillcott <samchillcott@gmail.com>
2021-06-11 12:19:34 +0100

    mongoose connection to db

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const morgan  = require('morgan');
-
+const mongoose = require('mongoose');
 
 const app = express();
 
-const dbURI = 'mongodb+srv://samchillcott:test1234@cluster0.pvxlr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://samchillcott:test1234@cluster0.pvxlr.mongodb.net/node-tuts?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then ((result) => app.listen(3000))
+    .catch ((error) => console.log(error));
 
 app.set('view engine', 'ejs');
-
-app.listen(3000);
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
