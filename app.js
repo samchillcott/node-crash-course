6dc884003e3e17aef1e4268ff0e78c332bacc525
Sam Chillcott <samchillcott@gmail.com>
2021-06-11 13:26:43 +0100

    add get methods

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan  = require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
 const dbURI = 'mongodb+srv://samchillcott:test1234@cluster0.pvxlr.mongodb.net/node-tuts?retryWrites=true&w=majority'
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then ((result) => app.listen(3000))
     .catch ((error) => console.log(error));
@@ -13,6 +15,28 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my blog'
+    })
+
+    blog.save()
+        .then((result) =>{
+            res.send(result);
+        })
+        .catch((error) => console.log(error));
+})
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((error) => console.log(error));
+})
 
 app.get('/', (req, res) => {
     const blogs = [
--- models/blog.js
+++ models/blog.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const blogSchema = newSchema({
+const blogSchema = new Schema({
     title: {
         type: 'string',
         required: true
