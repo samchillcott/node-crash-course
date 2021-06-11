7528e77623ac1adfda551f670a5e76c326b9f347
Sam Chillcott <samchillcott@gmail.com>
2021-06-11 09:49:22 +0100

    mongo setup and connection

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan  = require('morgan');
+
 
 const app = express();
+
+const dbURI = 'mongodb+srv://samchillcott:test1234@cluster0.pvxlr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.set('view engine', 'ejs');
 
 app.listen(3000);
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
