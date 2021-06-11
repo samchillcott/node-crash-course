85f27ff7db12be2ed7307bbb0a9a164d873938ed
Sam Chillcott <samchillcott@gmail.com>
2021-06-11 13:46:05 +0100

    output result to view

--- app.js
+++ app.js
@@ -16,48 +16,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my blog'
-    })
-
-    blog.save()
-        .then((result) =>{
-            res.send(result);
-        })
-        .catch((error) => console.log(error));
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((error) => console.log(error));
-})
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById("60c353a83619f491333d6c76")
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((error) => console.log(error));
-})
-
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-    res.render('index', {title: 'Home', blogs});
+    res.redirect('./blogs');
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'});
 })
+
+// Blog Routes
+
+app.get('/blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((error) => console.log(error));
+})
+
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create Blog'})
