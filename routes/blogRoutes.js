003d8e79445fb742f531268e3a6734e18ed9b209
Sam Chillcott <samchillcott@gmail.com>
2021-06-12 09:14:10 +0100

    move all controllers to controller file

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -7,45 +7,12 @@ const router = express.Router();
 
 router.get('/', blogController.blog_index);
 
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+router.get('/create', blogController.blog_create_get);
 
+router.post('/', blogController.blog_create_post);
 
-router.post('/', (req, res) => {
-  // console.log(req.body);
-  const blog = new Blog(req.body);
+router.get('/:id', blogController.blog_details);
 
-  blog.save()
-    .then(result => {
-      res.redirect('/blogs');
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then(result => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then(result => {
-      res.json({ redirect: '/blogs' });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
